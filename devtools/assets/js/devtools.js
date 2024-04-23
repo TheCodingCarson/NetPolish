f1fad6e2ce6a10eaac946be0e69d13812ed041b6
Coding Carson <coding@carson.rip>
2024-04-23 13:19:42 -0700

    •Restructuring for a Multi Browser Repo Layout

--- devtools/assets/js/devtools.js
+++
@@ -1,9 +0,0 @@
-// Devtools - Create Console Panel //
-chrome.devtools.panels.create(
-    "NetPolish",
-    "/devtools/assets/images/icon128.png",
-    "/devtools/devpanel.html", // HTML page for the content of the DevTools tab
-    function(panel) {
-        // Code invoked when the panel is created
-    }
-);
